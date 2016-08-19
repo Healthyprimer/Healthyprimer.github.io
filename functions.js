92ebbd9b21c5191c7af418a8023c6d0e9fd23032
Italia <i_danaji@hotmail.com>
2016-08-19 11:41:57 -0500

    Esta es mi primer version

---
+++ functions.js
@@ -0,0 +1,55 @@
+//jquery
+$(document).ready(function(){
+	$("#mko").materialbox();
+	//$("#imgsecundaria").materialbox();
+	//$(".bigimg").materialbox();
+	//jquery
+
+
+
+});
+
+//jquery
+$(document).ready(function(){
+	$("#bts").materialbox();
+	//$("#imgsecundaria").materialbox();
+	//$(".bigimg").materialbox();
+	//jquery
+
+
+
+});
+
+//jquery
+$(document).ready(function(){
+	$("#exo").materialbox();
+	//$("#imgsecundaria").materialbox();
+	//$(".bigimg").materialbox();
+	//jquery
+
+
+
+});
+
+
+//jquery
+$(document).ready(function(){
+	$("#shinee").materialbox();
+	//$("#imgsecundaria").materialbox();
+	//$(".bigimg").materialbox();
+	//jquery
+
+
+
+});
+
+//jquery
+$(document).ready(function(){
+	$("#ftisland").materialbox();
+	//$("#imgsecundaria").materialbox();
+	//$(".bigimg").materialbox();
+	//jquery
+
+
+
+});
